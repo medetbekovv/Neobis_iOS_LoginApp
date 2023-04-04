609d4dcf47bdfc1697b0c2b2830033cdace71761
Sultan <medetbekovs05.gmail.com>
2023-04-04 21:34:56 +0600

    Initial Commit

---
+++ Neobis_iOS_LoginApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Neobis_iOS_LoginApp
+//
+//  Created by user on 4/4/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
